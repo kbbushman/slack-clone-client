6b3029de70d778601cf04472170a61fae008a6a9
kbbushman <k.bushman@yahoo.com>
2019-05-27 20:13:11 -0700

    Adds react router, apollo provider, and initial test query

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from 'react-apollo';
+import Routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/routes/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+const Home = () => <Query query={gql`
+        {
+          allUsers {
+            id
+            username
+            email
+          }
+        }
+      `}>
+        {({ loading, error, data }) => loading
+          ? <h1>Loading...</h1>
+          : data.allUsers.map(user => <h1 key={user.id}>{user.email}</h1>)
+        }
+      </Query>
+
+export default Home;
