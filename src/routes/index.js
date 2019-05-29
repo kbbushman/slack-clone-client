e5ca0c5bf2db0e02176fda2224b96dccc735b605
kbbushman <k.bushman@yahoo.com>
2019-05-29 09:00:08 -0700

    Adds create team route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
+import CreateTeam from './CreateTeam';
 
 export default () => (
   <Router>
@@ -10,6 +11,7 @@ export default () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/create-team" component={CreateTeam} />
     </Switch>
   </Router>
 );
