d01ecc42268637b98dc614ad2670975218bb5e2e
kbbushman <k.bushman@yahoo.com>
2019-05-30 13:34:36 -0700

    Updates gql import from apollo-boost to graphql-tag

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Query } from 'react-apollo';
-import { gql } from 'apollo-boost';
+import gql from 'graphql-tag';
 
 const Home = () => <Query query={gql`
         {
