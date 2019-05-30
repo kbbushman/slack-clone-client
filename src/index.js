9182e65bc0031ab9ddd5ae33785acce36737181d
kbbushman <k.bushman@yahoo.com>
2019-05-30 08:45:28 -0700

    Migrates from apollo-boost to apollo-client v2 for afterware capabilities

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import Routes from './routes';
 import 'semantic-ui-css/semantic.min.css';
 import * as serviceWorker from './serviceWorker';
 
+
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  link: createHttpLink({ uri: 'http://localhost:5000/graphql'}),
+  cache: new InMemoryCache(),
 });
 
 const App = (
