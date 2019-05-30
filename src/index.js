e77b361ef85b811adf1692010c2e954987c5bcc4
kbbushman <k.bushman@yahoo.com>
2019-05-30 10:32:04 -0700

    Adds apollo middleware for setting req headers, and afterware for updating stored token with response header

--- src/index.js
+++ src/index.js
@@ -2,15 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import Routes from './routes';
 import 'semantic-ui-css/semantic.min.css';
 import * as serviceWorker from './serviceWorker';
 
+const httpLink = createHttpLink({ uri: 'http://localhost:5000/graphql' });
+
+const middlewareLink = setContext(() => ({
+  headers: {
+    'x-token': localStorage.getItem('sc_token'),
+    'x-refreshToken': localStorage.getItem('sc_refreshToken'),
+  }
+}));
+
+const afterwareLink = new ApolloLink((operation, forward) =>
+  forward(operation).map((response) => {
+    const { response: { headers } } = operation.getContext();
+    console.log(headers.get('x-token'));
+    if (headers) {
+      const token = headers.get('x-token');
+      const refreshToken = headers.get('x-refreshToken');
+
+      if (token) {
+        localStorage.setItem('sc_token', token);
+      }
+
+      if (refreshToken) {
+        localStorage.setItem('sc_refreshToken', refreshToken);
+      }
+    }
+    console.log('AFTERWARE RAN... Token = ', headers.get('x-token'));
+    return response;
+}));
+
+const link = afterwareLink.concat(middlewareLink.concat(httpLink));
 
 const client = new ApolloClient({
-  link: createHttpLink({ uri: 'http://localhost:5000/graphql'}),
+  link,
   cache: new InMemoryCache(),
 });
 
