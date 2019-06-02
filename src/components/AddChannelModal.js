5e70d7cc1707e7ea3134f9039b94890944872887
kbbushman <k.bushman@yahoo.com>
2019-06-02 15:34:41 -0700

    Adds create channel form with formik

--- src/components/AddChannelModal.js
+++ src/components/AddChannelModal.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { Form, Button, Modal, Input } from 'semantic-ui-react';
+import { withFormik } from 'formik';
 
-const AddChannelModal = ({ open, onClose }) => (
+const AddChannelModal = ({
+  open,
+  onClose,
+  values,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting,
+}) => (
   <Modal open={open} onClose={onClose}>
     <Modal.Header>Add Channel</Modal.Header>
       <Modal.Content>
         <Form>
           <Form.Field>
-            <Input fluid placeholder='Channel Name' />
+            <Input value={values.name} onChange={handleChange} onBlur={handleBlur} name='name' fluid placeholder='Channel Name' />
           </Form.Field>
           <Form.Group widths="equal">
-            <Button fluid>Cancel</Button>
-            <Button fluid>Create Channel</Button>
+            <Button onClick={onClose} disabled={isSubmitting} fluid>Cancel</Button>
+            <Button onClick={handleSubmit} disabled={isSubmitting} fluid>Create Channel</Button>
           </Form.Group>
         </Form>
       </Modal.Content>
   </Modal>
 );
 
-export default AddChannelModal;
+export default withFormik({
+  mapPropsToValues: () => ({ name: '' }),
+  handleSubmit: (values, { props, setSubmitting }) => {
+    console.log(values);
+    console.log('Submitting...');
+    setSubmitting(false);
+  },
+})(AddChannelModal);
