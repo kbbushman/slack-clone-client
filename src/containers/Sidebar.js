afe45c2ef759aa42af05790d8a60bea01cd81f05
kbbushman <k.bushman@yahoo.com>
2019-06-02 14:34:09 -0700

    Refactors sidebar to class component

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import findIndex from 'lodash/findIndex';
 import decode from 'jwt-decode';
 import gql from 'graphql-tag';
@@ -6,55 +6,63 @@ import { Query } from 'react-apollo';
 import Channels from '../components/Channels';
 import Teams from '../components/Teams';
 
-const Sidebar = ({ currentTeamId }) => {
-  return (
-    <Query query={gql`
-      {
-        allTeams {
-          id
-          name
-          channels {
+class Sidebar extends Component {
+  state = {
+    openAddChannelModal: false,
+  }
+
+  render() {
+    return (
+      <Query query={gql`
+        {
+          allTeams {
             id
             name
+            channels {
+              id
+              name
+            }
           }
         }
-      }
-    `}>
-      {
-        ({ loading, error, data: { allTeams } }) => {
-          if (loading) {
-            return <h1>Loading...</h1>;
+      `}>
+        {
+          ({ loading, error, data: { allTeams } }) => {
+            const { currentTeamId } = this.props;
+
+            if (loading) {
+              return <h1>Loading...</h1>;
+            }
+
+            console.log(allTeams);
+
+            const teamIndex = currentTeamId ? findIndex(allTeams, ['id', parseInt(currentTeamId, 10)]) : 0;
+            const team = allTeams[teamIndex];
+            let username = '';
+
+            try {
+              const token = localStorage.getItem('sc_token');
+              const { user } = decode(token);
+              username = user.username;
+            } catch (err) {}
+
+            return [
+              <Teams key="team-sidebar" teams={allTeams.map(team => ({
+                id: team.id,
+                letter: team.name.charAt(0).toUpperCase(),
+              }))} />,
+              <Channels
+                key="channels-sidebar"
+                teamName={team.name}
+                username={username}
+                channels={team.channels}
+                users={[{ id: 1, name: 'Slackbot'}, { id: 2, name: 'User1' }]}
+              />
+            ];
           }
-
-          console.log(allTeams);
-
-          const teamIndex = currentTeamId ? findIndex(allTeams, ['id', parseInt(currentTeamId, 10)]) : 0;
-          const team = allTeams[teamIndex];
-          let username = '';
-
-          try {
-            const token = localStorage.getItem('sc_token');
-            const { user } = decode(token);
-            username = user.username;
-          } catch (err) {}
-
-          return [
-            <Teams key="team-sidebar" teams={allTeams.map(team => ({
-              id: team.id,
-              letter: team.name.charAt(0).toUpperCase(),
-            }))} />,
-            <Channels
-              key="channels-sidebar"
-              teamName={team.name}
-              username={username}
-              channels={team.channels}
-              users={[{ id: 1, name: 'Slackbot'}, { id: 2, name: 'User1' }]}
-            />
-          ];
         }
-      }
-    </Query>
-  )
-}
+      </Query>
+    );
+  };
+};
 
 export default Sidebar;
